e43064b8ff8699ff18d85e130c3e34ae86a300f3
Caio Norder <caio@caionorder.com>
2017-03-02 09:32:30 -0300

    Baixar varias musicas

--- crawler/index.js
+++ crawler/index.js
@@ -1,6 +1,7 @@
+#!/usr/bin/env node
 const fs = require(`fs`)
 const rp = require(`request-promise`)
-const PATH = __dirname + '/musics'
+const PATH = '/Users/caionorder/Music/download/'
 const find = process.argv.filter(el => !el.includes('/')).join('+')
 const page = `&page=0`
 const BASE = `http://slider.kz`
@@ -9,25 +10,15 @@ const uri = `${BASE}/new/include/vk_auth.php?act=source1&q=${find}`
 const events = require('events')
 const eventEmitter = new events.EventEmitter()
 
-// const downloadedArr = []
-
-// const downloaded = (music) => {
-    
-// }
-
-// eventEmitter.on('downloaded', downloaded);  
-
-
 function decodeHTMLEntities (str) {
   if(str && typeof str === 'string') {
-    // strip script/html tags
     str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '');
     str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '');
     str = str.replace(/&#x[A-Z][0-9];/gmi, '');
   }
-
   return str;
 }
+
 const ensureExists = (path, mask, cb) => {
     if (typeof mask == 'function') {
         cb = mask
@@ -50,8 +41,6 @@ var getLinks = {
 console.time('tempo para receber a resposta')
 console.log(`\n\n\n\t\t INICIANDO A BUSCA PARA: ${find} `)
 
-
-
 rp(getLinks)
     .then(function(response) {
         console.timeEnd('tempo para receber a resposta')
@@ -60,49 +49,43 @@ rp(getLinks)
         const list = JSON.parse(response.trim())
 
         console.log(`\n\t\t recebi a lista de ${list.feed.length} mp3s ... `)
-        console.log(`\n\t\t MAS BAIXAREI APENAS 1 `)
-        // console.log(`\n\t\t agora salvarei no banco ... `)
-        // console.log(`\n\t\t enquanto baixo TODAS ELAS ... `)
 
         const listToSave = list.feed.map( el => el.entry )
+        const totalMp3 = list.feed.length
+        const total = 1
 
-        const musics = listToSave.slice(0,1).map( el => {
-            // console.log('el', el)
-            const PATH = __dirname +'/musics/'+el.artist.replace('/', '_')
-            const cb = (err) =>
-                err 
-                    ? console.log('Nao rolou criar as pastas aqui', err)
-                    : rp.get(`${BASE}${el.url}`)
-                        .on(`response`, res => {
-                            console.time(`tempo para baixar ${el.tit_art}.mp3`)
-                            console.log(`\n\t\t baixando ${el.tit_art} ... `)
-                        })
-                        .on(`error`, (err) =>
-                            console.log(`MERDA AO BAIXAR DE: ${BASE}${el.url} \n`, el.tit_art))
-                        .pipe(fs.createWriteStream(PATH+'/'+decodeHTMLEntities(el.tit_art+'.mp3')))
-                        .on( `finish`, () => {
-                            console.log(`\t\t\t Baixada: ${el.tit_art}.mp3`)
-                            console.timeEnd(`tempo para baixar ${el.tit_art}.mp3`)
-                            // process.exit(1) 
-                        })
+        mp3Down = function(total,listToSave){
+            const musics = listToSave.splice(0,total).map( el => {
+                const PATH = '/Users/caionorder/Music/download/'+el.artist.replace('/', '_');
+                const cb = (err) =>
+                    err
+                        ? console.log('ERRO AO CRIAR A PASTA', err)
+                        : rp.get(`${BASE}${el.url}`)
+                            .on(`response`,res => {
+                                console.time(`tempo para baixar ${el.tit_art}.mp3`)
+                                console.log(`\n\t\t baixando ${el.tit_art} ... `)
+                            })
+                            .on(`error`, (err) =>
+                                console.log(`ERRO AO BAIXAR DE: ${BASE}${el.url} \n`, el.tit_art))
+                            .pipe(fs.createWriteStream(PATH+'/'+decodeHTMLEntities(el.tit_art+'.mp3')))
+                            .on( `finish`, () => {
+                                console.log(`\t\t\t Baixada: ${el.tit_art}.mp3`)
+                                console.timeEnd(`tempo para baixar ${el.tit_art}.mp3`)
+                                total++
+                                mp3Down(total,listToSave)
+                            })
+                ensureExists( PATH, 0744, cb)
+            })
 
-            // console.log('PATH', PATH)
-            ensureExists( PATH, 0744, cb)
         }
-        )
+
+        mp3Down(total,listToSave)
+
         return listToSave
     })
     .then( body => {
-        // console.log(`\n\n\n\t\t SALVEI A PORRA TODA NO BANCO`, body)
-        // console.timeEnd('tempo para baixar TODAS as musicas')
-        // process.exit(1) 
 
     })
     .catch( err => {
-        // API call failed... 
         console.log(`err`, err)
     })
-    // .finally( () => {
-    //     // API call failed... 
-    //     console.log(`Por hoje eh soh pesoal!`)
-    // })
