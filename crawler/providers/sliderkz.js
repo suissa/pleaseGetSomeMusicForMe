3fb7a777a9dbf9e3adf24d8b663225568a2a6841
Rodrigo Santiago <rsantiago897@gmail.com>
2017-03-12 05:37:42 -0300

    ajustando identação e modificações mínimas

--- crawler/providers/sliderkz.js
+++ crawler/providers/sliderkz.js
@@ -5,7 +5,7 @@ const rp = require(`request-promise`)
 const BASE = `http://slider.kz`
 
 const getLinks = {
-	uri: '',
+  uri: '',
   headers: {
       'Content-Type': `application/json`
   },
@@ -14,12 +14,12 @@ const getLinks = {
 
 function SliderKZ () {
 
-	this.url = `${BASE}/new/include/vk_auth.php?act=source1&q=`
-	this.page = `&page=0`
+  this.url = `${BASE}/new/include/vk_auth.php?act=source1&q=`
+  this.page = `&page=0`
 
-	this.buildSongs = (response) => {
+  this.buildSongs = (response) => {
 
-		console.timeEnd('tempo para receber a resposta')
+    console.timeEnd('tempo para receber a resposta')
     if ( ! response.trim().length ) return console.log('Não achou essa busca!')
 
     const list = JSON.parse(response.trim())
@@ -30,18 +30,45 @@ function SliderKZ () {
     var p = new Promise()
 
     return p.resolve(list.feed.map( el => el.entry ))
-	}
+  }
 }
 
 SliderKZ.prototype.search = function (query) {
 
-	getLinks.uri = [this.url, query].join('');
+  getLinks.uri = [this.url, query].join('');
 
-	let songs = rp(getLinks)
-		.then(this.buildSongs)
-		//.catch(err => Promise.reject(err))
+  return Promise.resolve(rp(getLinks)
+    .then(body => {
+      return {
+        songs: self.buildSongs(body),
+        download: self.prepareForDownload
+      }
+    })
+    .catch(err => err))
+}
 
-	return songs
+SliderKZ.prototype.prepareForDownload = function (title, uri, path) {
+
+  let self = this
+  getLinks.uri = uri
+
+  return Promise.resolve(
+    rp.get(getLinks)
+    .on('response', res => {
+          console.time(`tempo para baixar ${title}.mp3 de SliderKZ`)
+          console.log(`\n\t\t baixando ${title} ... `)
+        })
+        .on(`error`, (err) =>
+          console.log(`MERDA AO BAIXAR DE: ${songUrl} \n`, title))
+      //.pipe(fs.createWriteStream())
+      .pipe(fs.createWriteStream(path))
+      .on( `finish`, () => {
+          console.log(`\t\t\t Baixada: ${title}.mp3`)
+          console.timeEnd(`tempo para baixar ${title}.mp3 de SliderKZ`)
+      })
+  )
+      
+
 }
 
 module.exports = new SliderKZ()
