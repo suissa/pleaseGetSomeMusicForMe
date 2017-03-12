c0cc70260faf4fae86984a3cd867ef30e441d713
Rodrigo Santiago <rsantiago897@gmail.com>
2017-03-12 05:40:14 -0300

    ajustando identação

--- crawler/providers/musicpleer.js
+++ crawler/providers/musicpleer.js
@@ -6,81 +6,81 @@ const rp = require(`request-promise`)
 const BASE = `http://databrainz.com`
 
 const getLinks = {
-	uri: '',
-	headers: {
-		'HOST': 'databrainz.com',
-		'Referer': 'http://musicpleer.audio/',
-		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
-	}
+  uri: '',
+  headers: {
+    'HOST': 'databrainz.com',
+    'Referer': 'http://musicpleer.audio/',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+  }
 }
 
 function MusicPleer () {
 
-	this.urlSearch = `${BASE}/api/search_api.cgi?format=json&mh=50&where=mpl&r=&_=1489228409592&qry=`
-	this.urlSong = `${BASE}/api/data_api_new.cgi?&r=mpl&format=json&_=1489228409608&id=`
-	this.page = `&page=0`
+  this.urlSearch = `${BASE}/api/search_api.cgi?format=json&mh=100&where=mpl&r=&_=1489228409592&qry=`
+  this.urlSong = `${BASE}/api/data_api_new.cgi?&r=mpl&format=json&_=1489228409608&id=`
+  this.page = `&page=0`
 
-	this.buildSongs = (response) => {
+  this.buildSongs = (response) => {
 
-		var self = this
+    var self = this
 
-		let list = JSON.parse(response).results
+    let list = JSON.parse(response).results
 
-		let songs = list.map(song => {
-			let album = (song.album != '') ? ' - ' + song.album : ''
+    console.log(`\n\t\t Recebi a lista de ${list.length} mp3s ... `)
+    console.log(`\n\t\t MAS BAIXAREI APENAS AS QUE VC ESCOLHER! `)
 
-			song.tit_art = song.title + ' - ' + song.artist + album
-			song.url = this.urlSong + song.url
-			return song
-		})
+    let songs = list.map(song => {
+      let album = (song.album != '') ? ' - ' + song.album : ''
 
-		return songs
-			
-	}
+      song.tit_art = song.title + ' - ' + song.artist + album
+      song.url = this.urlSong + song.url
+      return song
+    })
+
+    return songs 
+  }
 
 }
 
 MusicPleer.prototype.search = function (query) {
 
-	let self = this
+  let self = this
 
-	getLinks.uri = [this.urlSearch, query].join('');
+  getLinks.uri = [this.urlSearch, query].join('');
 
-	return Promise.resolve(rp(getLinks)
-		.then(body => {
-			return {
-				songs: self.buildSongs(body),
-				download: self.prepareForDownload
-			}
-		})
-		.catch(err => err))
+  return Promise.resolve(rp(getLinks)
+    .then(body => {
+      return {
+        songs: self.buildSongs(body),
+        download: self.prepareForDownload
+      }
+    })
+    .catch(err => err))
 
 }
 
 MusicPleer.prototype.prepareForDownload = function (title, uri, path) {
 
-	let self = this
-	getLinks.uri = uri
+  let self = this
+  getLinks.uri = uri
 
-	return Promise.resolve(
-		rp.get(getLinks)
-	      .then(res => {
-	      	rp(JSON.parse(res).song.url).on('response', res => {
-	          console.time(`tempo para baixar ${title}.mp3`)
-	          console.log(`\n\t\t baixando ${title} ... `)
-	      	})
-	      	.on(`error`, (err) =>
-		        console.log(`MERDA AO BAIXAR DE: ${songUrl} \n`, title))
-		    //.pipe(fs.createWriteStream())
-		    .pipe(fs.createWriteStream(path))
-		    .on( `finish`, () => {
-		        console.log(`\t\t\t Baixada: ${title}.mp3`)
-		        console.timeEnd(`tempo para baixar ${title}.mp3`)
-		    })
-	      })
-	)
-      
-
+  return Promise.resolve(
+    rp.get(getLinks)
+      .then(res => {
+        rp(JSON.parse(res).song.url).on('response', res => {
+          console.time(`tempo para baixar ${title}.mp3 de MusicPleer`)
+          console.log(`\n\t\t baixando ${title} ... `)
+        })
+        .on(`error`, (err) =>
+          console.log(`MERDA AO BAIXAR DE: ${songUrl} \n`, title))
+      //.pipe(fs.createWriteStream())
+      .pipe(fs.createWriteStream(path))
+      .on( `finish`, () => {
+          console.log(`\t\t\t Baixada: ${title}.mp3`)
+          console.timeEnd(`tempo para baixar ${title}.mp3  de MusicPleer`)
+      })
+    })
+  )
 }
 
 
