3512f757684ebe4e25d381816800eb94e4019a43
suissa <jnascimento@gmail.com>
2019-06-01 15:23:25 -0300

    modularizada a fn buildSongs

--- providers/sliderkz.js
+++ providers/sliderkz.js
@@ -15,29 +15,30 @@ const getLinks = {
   json: true
 }
 
+const buildSongs = (response) => {
+
+  if ( ! response.audios ) return console.log('Songs not found!')
+
+  let pageCount = Object.keys(response.audios)[0]
+  const list = response.audios[Object.keys(response.audios)[0]]
+
+  const p = new Promise((resolve, reject) => {
+
+      let newList = list.map(s => {
+          s.url = `${BASE}/download/${s.id}/${s.duration}/${s.url}/${s.tit_art}.mp3?extra=${s.extra}`
+          s.provider = 'SliderKZ'
+          return s
+      })
+      resolve(newList)
+  })
+
+  return p.then((resp) => resp)
+}
+
 const PROVIDER = () => {
   let url = `${BASE}/vk_auth.php?&q=`
   let page = `&page=0`
 
-  const buildSongs = (response) => {
-
-    if ( ! response.audios ) return console.log('Songs not found!')
-
-    let pageCount = Object.keys(response.audios)[0]
-    const list = response.audios[Object.keys(response.audios)[0]]
-
-    const p = new Promise((resolve, reject) => {
-
-        let newList = list.map(s => {
-            s.url = `${BASE}/download/${s.id}/${s.duration}/${s.url}/${s.tit_art}.mp3?extra=${s.extra}`
-            s.provider = 'SliderKZ'
-            return s
-        })
-        resolve(newList)
-    })
-
-    return p.then((resp) => resp)
-  }
 
 
   const prepareForDownload = (title, uri, path, index) => {
@@ -107,88 +108,4 @@ const PROVIDER = () => {
   return lib
 }
 
-// function SliderKZ () {
-
-//   this.url = `${BASE}/vk_auth.php?&q=`
-//   this.page = `&page=0`
-
-//   this.buildSongs = (response) => {
-
-//     if ( ! response.audios ) return console.log('Songs not found!')
-
-//     let pageCount = Object.keys(response.audios)[0]
-//     const list = response.audios[Object.keys(response.audios)[0]]
-
-//     var p = new Promise((resolve, reject) => {
-
-//         let newList = list.map(s => {
-//             s.url = `${BASE}/download/${s.id}/${s.duration}/${s.url}/${s.tit_art}.mp3?extra=${s.extra}`
-//             s.provider = 'SliderKZ'
-//             return s
-//         })
-//         resolve(newList)
-//     })
-
-//     return p.then((resp) => resp)
-//   }
-// }
-
-// SliderKZ.prototype.search = function (query) {
-
-//   getLinks.uri = [this.url, query].join('');
-
-//   let self = this
-//   return Promise.resolve(rp(getLinks)
-//     .then(body => {
-//       return {
-//         songs: self.buildSongs(body),
-//         download: self.prepareForDownload
-//       }
-//     })
-//     .catch(err => Promise.reject(err)))
-// }
-
-// SliderKZ.prototype.prepareForDownload = function (title, uri, path, index) {
-
-//   let self = this
-//   getLinks.uri = uri
-
-//   return new Promise((resolve, reject) => {
-//       rp.get(getLinks)
-//       .on('response', res => {
-//             var len = parseInt(res.headers['content-length'], 10);
-
-//             let s = title + ': \n'
-//             multi.write(s);
-//             var bar = multi(40, 3 + index, {
-//                 width : 20,
-//                 solid : {
-//                     text : ' ',
-//                     foreground : 'white',
-//                     background : 'blue'
-//                 },
-//                 empty : { text : ' ' },
-//             });
-
-//             var total = 0
-//             res.on('data', function (chunk) {
-//                 total += (chunk.byteLength * 100)  / len;
-//                 bar.percent(parseInt(Math.round(total)));
-//             });
-
-//             res.on('end', function () {
-//                 resolve()
-//             });
-//           })
-//           .on(`error`, (err) =>
-//             multi.write(`got a problem downloading: ${title} \n`))
-//         //.pipe(fs.createWriteStream())
-//         .pipe(fs.createWriteStream(path))
-//         .on( `finish`, () => {
-//         })
-//   })
-
-// }
-
-// module.exports = new SliderKZ()
 module.exports = PROVIDER()
